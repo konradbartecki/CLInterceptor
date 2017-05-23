1135bacebfb01fa2233cf7c4340df9fd85897610
Konrad Bartecki <konrad.bartecki@edge1s.com>
2017-05-23 13:21:29 +0200

    Release

--- CLInterceptor/Program.cs
+++ CLInterceptor/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
+using NLog.Common;
 
 namespace CLInterceptor
 {
@@ -10,6 +14,24 @@ namespace CLInterceptor
     {
         static void Main(string[] args)
         {
+            var logger = LogManager.GetCurrentClassLogger();
+
+            var config = ConfigurationManager.OpenExeConfiguration("CLInterceptor.exe.config");
+            
+            var targetExecutable = config.AppSettings.Settings["TargetExecutable"].Value;
+            var arguments = string.Join(" ", args);
+
+            var startinfo = new ProcessStartInfo(targetExecutable, arguments)
+            {
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+
+            logger.Info($"Starting task \"{targetExecutable}\" with arguments \"{arguments}\"");
+            logger.Factory.Flush();
+            Process.Start(startinfo);
 
         }
     }
